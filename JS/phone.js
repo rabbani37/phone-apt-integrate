e7a6a3b8db64c8a868630412b22e86e289e3714d
georabbani <mrabbbani08@gmail.com>
2026-01-08 09:34:23 +0600

    complete style 'search your phone' section

--- JS/phone.js
+++ JS/phone.js
@@ -1,18 +1,40 @@
 // data load from API;
 const url = "https://openapi.programming-hero.com/api/phones?search=iphone"
 const dataLoadFromApi = async () => {
-    const response = await fetch(url) 
+    const response = await fetch(url)
     const apiData = await response.json()
     const phones = apiData.data;
-    // console.log(phones);
     displayPhone(phones)
-
+    
 }
 
-const displayPhone= phones=>{
-    // console.log(phones)
+const displayPhone = phones => {
+    const phonesContainer = document.getElementById("phones_container")
     phones.forEach(phone => {
-        console.log(phone)
+        console.log(phones);
+        const {brand,image,phone_name,slug} = phone;
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = "card bg-base-200 w-full  shadow-sm p-5 border border-gray-300"
+        phoneCard.innerHTML=`
+             <figure>
+                        <img src="${image}" />
+                    </figure>
+                    <div class="card-body text-center">
+
+                        <h2 class="text-2xl font-bold text-center"> ${phone_name}</h2>
+                        <div class="font-semibold text-gray-500 text-lg  ">
+                            <p>Brand: <span class="text-black"> ${brand}</span> </p>
+                            <p>Model:<span class="text-black"> ${slug}</span>  </p>
+                        </div>
+                        <p class="text-gray-600 font-semibold">A card component has a figure, a body part, and inside
+                            body there are title and actions parts</p>
+
+                        <div class="card-actions justify-center">
+                            <button class="btn w-3/5  btn-primary">Show Details</button>
+                        </div>
+                    </div>
+        `;
+        phonesContainer.appendChild(phoneCard)
     });
 }
 
